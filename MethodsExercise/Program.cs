a17cbfca89cc7631888fd4a7048406fa7c9bae92
Andy Villalobos <Andy.villa012@gmail.com>
2023-01-29 12:03:44 -0800

    finished profile iteration one

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -4,7 +4,22 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, what is your name?");
+            var name = Console.ReadLine();
+
+            Console.WriteLine("What is your favorite color?");
+            var favColor = Console.ReadLine();
+
+            Console.WriteLine("What is your favorite animal?");
+            var favAnimal = Console.ReadLine();
+
+            Console.WriteLine("What is your favorite Band?");
+            var favBand = Console.ReadLine();
+
+            Console.WriteLine($"Thanks, {name}! Here is your profile:");
+
+            Console.WriteLine($"Your name is {name}, your favorite color is {favColor} and your favorite animal is {favAnimal}. We also learned that your favorite band is {favBand}!");
         }
+
     }
 }
